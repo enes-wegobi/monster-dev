e4c37fc35b64c8356b81b66415bdc5042ef8d427
enojen <enesozel.eo@gmail.com>
2023-11-18 12:53:30 +0100

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  });
   app.use(helmet());
   await app.listen(3000);
 }
